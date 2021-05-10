7f31b89669bc63c70dca538b111f1a1fe0625520
aayushigandhi8 <aayushi.gandhi@bacancy.com>
2021-05-10 17:06:00 +0530

    modified api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,9 @@ import { GmapComponent } from './view/gmap/gmap.component';
     BrowserModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
-      // apiKey: 'AIzaSyDPwMsQS3LMhE2sX5fIKp4yMBWbBQh7x8Q',
-      apiKey:'AIzaSyCmkGT-2rPU9nfIRajokaiYXPm7SIMk11A',
+      // apiKey: 'AIzaSyDPwMsQS3LMhE2sX5fIKp4yMBWbBQh7x8Q', //1
+      // apiKey:'AIzaSyCmkGT-2rPU9nfIRajokaiYXPm7SIMk11A', //ref1
+      apiKey:'AIzaSyCaKbVhcX_22R_pRKDYuNA7vox-PtGaDkI', //ref2
       libraries: ['places']
     })
   ],
